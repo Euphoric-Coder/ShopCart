ae9dda989654eddb9ee64949fa2b3f210bd67497
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-09-11 22:57:25 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className=''>
+      Lorem ipsum dolor sit.
+    </div>
+  )
+}
+
+export default App
